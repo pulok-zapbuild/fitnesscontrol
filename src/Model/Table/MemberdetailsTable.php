951455febe77f0c9cc8559509714815a2e4f6d6d
pulok-zapbuild <pulok.zapbuild@gmail.com>
2020-03-23 07:17:17 +0530

    memberdetails table

---
+++ src/Model/Table/MemberdetailsTable.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class MemberDetailsTable extends Table
+{
+	public function initialize(array $config) : void
+	{	
+		parent::initialize($config);
+		$this->setTable('memberdetails');
+		$this->setDisplayField('user_id');
+		$this->setPrimaryKey('id');
+		$this->addBehavior('Timestamp', [
+            'events' => [
+                'Model.beforeSave' => [
+                    'created_at' => 'new',
+                    'modified_at' => 'always']]]);
+        $this->hasOne('users');
+    }
+
+	public function validationDefault(Validator $validator): Validator
+    {
+        
+    }
+}
